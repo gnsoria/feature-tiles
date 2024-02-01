384b0f1985bac05dd87b0eb3da80c177e4977f1b
Galen Soria <6798606+gnsoria@users.noreply.github.com>
2024-01-31 18:10:13 -0800

    TileGrid: Add a className prop and some padding to the bottom of the grid.

--- src/TileGrid/TileGrid.jsx
+++ src/TileGrid/TileGrid.jsx
@@ -3,9 +3,14 @@ import React from "react";
 import "./tile_grid.css"
 import DefaultTile from "../DefaultTile";
 
-export default function TileGrid({ allTileData, colorScheme = {}, renderTileText }) {
+export default function TileGrid({
+    allTileData,
+    className = "",
+    colorScheme = {},
+    renderTileText
+}) {
     return (
-        <div className="feature-tile-grid" >
+        <div className={`feature-tile-grid ${className}`} >
             {allTileData.map((tileData, index) =>
                 <div key={index} className="feature-tile-wrapper">
                     <DefaultTile
